6a4eebdd8a0239bfb2977e34c19f4a7d0ffbfe26
grebalexey <136621926+grebalexey@users.noreply.github.com>
2023-09-06 21:32:04 +0300

    функции

---
+++ 06.09/script.js
@@ -0,0 +1,54 @@
+// function nameFunction(){
+//     console.log("Привет мир")
+// }
+
+// nameFunction()
+
+
+// let xGlob = 123;
+// let yGlob = 100;
+// let strGlob = "Мир"
+// function nameFunction(x,y){
+//     console.log(x + y)
+// }
+
+// nameFunction(xGlob, yGlob)
+
+// let value = +prompt("Укажите число", "")
+
+// function factorial(n){
+//     if (n === 1){
+//         return n
+//     }
+//     return n*factorial(n - 1)
+// }
+
+// alert(factorial(value))
+
+
+// console.log($(".allParent"))
+
+// $("child11").click(function(){
+//     // $(this).toggleClass("isActive")
+//     $(this).parent().toggleClass("isActive")
+//     $(this).parent().next().toggleClass("isActive")
+
+    // $(this).closest(".allParent").toggleClass("isActive")
+    // $(this).closest(".allParent").find(".child44").toggleClass("isActive")
+    // $(this).parent().siblings().toggleClass("isActive")
+// })
+
+// .parent() - выход на родителя
+
+// .next() - следующий в узле
+
+// .prev() - предыдущий в узле
+
+// .children() - выход на потомка
+
+// .closest(".allParent") - выход на allParent
+
+// .find(".child44") - поиск элемента child44
+
+// .siblings() - все соседи
+
